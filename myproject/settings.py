4dd1e3e10205d4fe5c47ed0e4d9f381848752e90
sreyas <sreyassunandan2601@gmail.com>
2025-12-29 21:05:24 +0530

    second commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,9 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dr=@lts%9an&0-^zmg2oz=%o$^=7=4wr8h#)%wgfi*458mx#d&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "project01x.pythonanywhere.com",
+    "127.0.0.1",
+    "localhost",
+]
+
 
 
 # Application definition
